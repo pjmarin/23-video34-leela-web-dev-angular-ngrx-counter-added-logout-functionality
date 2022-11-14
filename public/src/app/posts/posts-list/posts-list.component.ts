6f994adc0736a34eddd3e0a02ba5cd6744041b96
pjmarin <39600116+pjmarin@users.noreply.github.com>
2022-11-14 17:14:43 +0100

    added logout functionality

---
+++ public/src/app/posts/posts-list/posts-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
+import { Post } from '../../models/posts.model';
+import { AppState } from '../../store/app.state';
+import { deletePost } from '../state/posts.actions';
+import { getPosts } from '../state/posts.selector';
+
+@Component({
+  selector: 'app-posts-list',
+  templateUrl: './posts-list.component.html',
+  styleUrls: ['./posts-list.component.css']
+})
+export class PostsListComponent implements OnInit {
+  posts: Observable<Post[]> = of([]);
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit(): void {
+    this.posts = this.store.select(getPosts);
+  }
+
+  onDeletePost(id: string) {
+    if(confirm("Are you sure you want to delete this post ?")) {
+      this.store.dispatch(deletePost({ id }));
+    }
+  }
+
+}
